3c1967df44352a1e7c28e6f0ed262630858d3ab0
VINXIS <oykxf2@gmail.com>
2023-09-18 18:07:58 -0600

    upgrade nuxt version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    target: "static",
+    ssr: false,
     app: {
         head: {
             charset: "utf-8",
