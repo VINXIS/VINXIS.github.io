1af8ee8544b70b5974312f448691aa90cd40bbdc
VINXIS <oykxf2@gmail.com>
2024-04-08 05:38:40 -0600

    upgrade nuxt version and add vercel/speedinsights

--- app.vue
+++ app.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="fade-in">
+        <SpeedInsights />
         <Particles />
         <NavigationBar />
         <NuxtPage />
     </div>
 </template>
+
+<script setup>
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+</script>
 
 <style>
 .fade-in {
